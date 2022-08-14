ee96669df29fb3a9d5da698cdb8e0212df755e36
MikkoK189 <63245862+MikkoK189@users.noreply.github.com>
2022-08-14 20:23:28 +0300

    Header.

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import "../styles/Header.css";
+
+const Header = () => {
+  return (
+    <nav className="header">
+      <div className="header--title">
+        <img src="./images/Troll Face.png"></img>
+        <h1>Meme Generator</h1>
+      </div>
+      <p>React Course - Project 3</p>
+    </nav>
+  );
+};
+
+export default Header;
