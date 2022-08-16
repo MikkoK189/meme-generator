d2b07a13a2f5ee85873101fd2c402c1562ac1eb6
MikkoK189 <mikkok11@gmail.com>
2022-08-16 22:21:11 +0300

    Small changes.

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,17 +3,11 @@ import memesData from "../memesData";
 
 const Meme = () => {
   function buttonClicked() {
-    const randIndex = Math.floor(Math.random() * 100); // Number between 0 and 100
-    const memeToDisplay = memesData.data.memes[randIndex];
+    const memesArray = memesData.data.memes;
+    const randIndex = Math.floor(Math.random() * memesArray.length); // Number between 0 and 100
+    const memeToDisplay = memesArray[randIndex];
     console.log(memeToDisplay.url);
   }
-  /**
-   * Challenge: Get a random image from the `memesData` array
-   * when the "new meme image" button is clicked.
-   *
-   * Log the URL of the image to the console. (Don't worry
-   * about displaying the image yet)
-   */
 
   return (
     <div className="meme--form">
@@ -22,6 +16,7 @@ const Meme = () => {
         <input type="text" placeholder="BOTTOM TEXT" />
       </div>
       <button onClick={buttonClicked}>Get a new meme image</button>
+      <img></img>
     </div>
   );
 };
