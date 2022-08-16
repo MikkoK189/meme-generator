b800246335162517a15abaf76154599c9a4683bf
MikkoK189 <mikkok11@gmail.com>
2022-08-16 22:12:22 +0300

    Getting a meme url from the local data.

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,14 +1,28 @@
 import "../styles/Meme.css";
+import memesData from "../memesData";
 
 const Meme = () => {
+  function buttonClicked() {
+    const randIndex = Math.floor(Math.random() * 100); // Number between 0 and 100
+    const memeToDisplay = memesData.data.memes[randIndex];
+    console.log(memeToDisplay.url);
+  }
+  /**
+   * Challenge: Get a random image from the `memesData` array
+   * when the "new meme image" button is clicked.
+   *
+   * Log the URL of the image to the console. (Don't worry
+   * about displaying the image yet)
+   */
+
   return (
-    <form className="meme--form">
+    <div className="meme--form">
       <div className="form--cont">
         <input type="text" placeholder="TOP TEXT" />
         <input type="text" placeholder="BOTTOM TEXT" />
       </div>
-      <button>Get a new meme image</button>
-    </form>
+      <button onClick={buttonClicked}>Get a new meme image</button>
+    </div>
   );
 };
 
